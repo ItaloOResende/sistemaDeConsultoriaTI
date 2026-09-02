ff1f94a0ba4d298768232dfb455265b327f366d6
Igor Quintino <igorcquintino@hotmail.com>
2026-09-02 19:54:14 -0300

    pequenas mudanças

--- buscaGeral.py
+++ buscaGeral.py
@@ -39,3 +39,9 @@ try:
    fazerTabela.prepara_tabela('preços.csv')
 except Exception as e:
     print(f"Erro ao gerar tabela: {e}")
+
+try:
+    filtrarTabela.filtrar_memoria_desktop('Produtos Ordenados.csv', phtipo, phmemoria)
+    pass
+except Exception as e:
+    print(f"Ocorreu um erro  ao filtrar tabela: {e}")
